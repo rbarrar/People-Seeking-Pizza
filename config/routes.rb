745a8fb6d7a6ccfcf8f7dacd18f82835de2305cf
Ryan Barrar <Rbarrar17@gmail.com>
2016-04-20 14:42:09 -0400

    fixed routing issue with user/home

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
-  get '/users' => 'users#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
